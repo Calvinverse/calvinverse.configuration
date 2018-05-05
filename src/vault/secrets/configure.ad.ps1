9e519bc4674c9cea4e06a7e41e36d8e1a0796cbb
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-05-05 23:02:50 +1200

    Fixing typo in the path for the AD user password

--- src/vault/secrets/configure.ad.ps1
+++ src/vault/secrets/configure.ad.ps1
@@ -1,2 +1,2 @@
-# write to secrets/environment/directory/users/bind, content is the password for the
+# write to secret/environment/directory/users/bind, content is the password for the
 # AD user that is used by nexus and jenkins to look up users and groups
