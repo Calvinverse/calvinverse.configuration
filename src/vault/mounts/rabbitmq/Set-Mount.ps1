d04f8ff4345337f88f5e672c69aa9623f7e93856
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-06-12 12:05:55 +1200

    Updating the way RabbitMQ roles are set in Vault

---
+++ src/vault/mounts/rabbitmq/Set-Mount.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param(
+    [string] $vaultPath,
+    [string] $serverAddress,
+    [string] $consulDomain = 'consulverse',
+    [string] $rabbitUser = 'user.vault',
+    [string] $rabbitPassword = ''
+)
+
+$ErrorActionPreference = 'Stop'
+
+. (Join-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) 'helpers.ps1')
+
+$enableRabbitMq = @(
+    'rabbitmq'
+)
+Invoke-Vault `
+    -vaultPath $vaultPath `
+    -serverAddress $serverAddress `
+    -command 'secrets enable' `
+    -arguments $enableRabbitMq
+
+$configureRabbitMq = @(
+    'rabbitmq/config/connection',
+    "connection_uri=`"http://http.queue.service.$($consulDomain):15672`"",
+    "username=`"$($rabbitUser)`"",
+    "password=`"$($rabbitPassword)`""
+)
+Invoke-Vault `
+    -vaultPath $vaultPath `
+    -serverAddress $serverAddress `
+    -command 'write' `
+    -arguments $configureRabbitMq
+
+$ttlRabbitMq = @(
+    'rabbitmq/config/lease',
+    'ttl=3600',
+    "max_ttl=86400"
+)
+Invoke-Vault `
+    -vaultPath $vaultPath `
+    -serverAddress $serverAddress `
+    -command 'write' `
+    -arguments $ttlRabbitMq
