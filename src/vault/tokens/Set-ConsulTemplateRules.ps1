fe4da6c3a8ef3b809abe8d8eaf6869afd4481b24
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-08-18 16:47:28 +1200

    Cleaning up the consul-template rule script

--- src/vault/tokens/Set-ConsulTemplateRules.ps1
+++ src/vault/tokens/Set-ConsulTemplateRules.ps1
@@ -8,11 +8,14 @@ $ErrorActionPreference = 'Stop'
 
 . (Join-Path (Split-Path $PSScriptRoot -Parent) 'helpers.ps1')
 
+$defaultLinuxPolicies = 'default' `
+    + ',rabbitmq.creds.write.vhost.logs.syslog' `
+    + ',secret.write.metrics.http'
 $createRole = @(
     'auth/token/roles/role.system.syslogandmetrics',
     'period=1h',
     'orphan=true',
-    'allowed_policies="default,rabbitmq.creds.write.vhost.logs.syslog,secret.write.metrics.http"'
+    "allowed_policies=$defaultLinuxPolicies"
 )
 Invoke-Vault `
     -vaultPath $vaultPath `
@@ -20,11 +23,14 @@ Invoke-Vault `
     -command 'write' `
     -arguments $createRole
 
+$defaultWindowsPolicies = 'default' `
+    + ',rabbitmq.creds.write.vhost.logs.file' `
+    + ',secret.write.metrics.http'
 $createRole = @(
     'auth/token/roles/role.system.filelogandmetrics',
     'period=1h',
     'orphan=true',
-    'allowed_policies="default,rabbitmq.creds.write.vhost.logs.file,secret.write.metrics.http"'
+    "allowed_policies=$defaultWindowsPolicies"
 )
 Invoke-Vault `
     -vaultPath $vaultPath `
@@ -32,11 +38,13 @@ Invoke-Vault `
     -command 'write' `
     -arguments $createRole
 
+$httpArtefactRules = $defaultLinuxPolicies `
+    + ',secret.environment.directory.bind'
 $createRole = @(
     'auth/token/roles/role.artefacts.http',
     'period=1h',
     'orphan=true',
-    'allowed_policies="default,rabbitmq.creds.write.vhost.logs.syslog,secret.environment.directory.bind"'
+    "allowed_policies=$httpArtefactRules"
 )
 Invoke-Vault `
     -vaultPath $vaultPath `
@@ -44,11 +52,14 @@ Invoke-Vault `
     -command 'write' `
     -arguments $createRole
 
+$masterBuildRules = $defaultLinuxPolicies `
+    + ',rabbitmq.creds.readwrite.vhost.build.trigger' `
+    + ',secret.environment.directory.bind'
 $createRole = @(
     'auth/token/roles/role.build.master',
     'period=1h',
     'orphan=true',
-    'allowed_policies="default,rabbitmq.creds.write.vhost.logs.syslog,secret.write.metrics.http,rabbitmq.creds.read.vhost.builds,secret.environment.directory.bind"'
+    "allowed_policies=$masterBuildRules"
 )
 Invoke-Vault `
     -vaultPath $vaultPath `
@@ -56,11 +67,27 @@ Invoke-Vault `
     -command 'write' `
     -arguments $createRole
 
+$windowsBuildAgentRules = $defaultWindowsPolicies `
+    + ',secret.environment.directory.users.build.agent'
+$createRole = @(
+    'auth/token/roles/role.build.agent.windows',
+    'period=1h',
+    'orphan=true',
+    "allowed_policies=$windowsBuildAgentRules"
+)
+Invoke-Vault `
+    -vaultPath $vaultPath `
+    -vaultServerAddress $vaultServerAddress `
+    -command 'write' `
+    -arguments $createRole
+
+$dashboardMetricsRules = $defaultLinuxPolicies `
+    + ',secret.environment.directory.bind'
 $createRole = @(
     'auth/token/roles/role.metrics.dashboards',
     'period=1h',
     'orphan=true',
-    'allowed_policies="default,rabbitmq.creds.write.vhost.logs.syslog,secret.write.metrics.http,secret.environment.directory.bind"'
+    "allowed_policies=$dashboardMetricsRules"
 )
 Invoke-Vault `
     -vaultPath $vaultPath `
