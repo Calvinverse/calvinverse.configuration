d9bca7b18adcc8427e6b616d7ab6fa3e15686872
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-06-26 19:11:36 +1200

    Fixing the keys again

--- consul-kv-service-proxy/main.tf
+++ consul-kv-service-proxy/main.tf
@@ -1,15 +1,15 @@
 resource "consul_key_prefix" "consul-kv-service-proxy" {
   token      = var.consul_acl_token
 
-  path_prefix = "config/services/proxy.edge/"
+  path_prefix = "config/services/proxy.edge"
 
   subkeys = {
-    "ui/color"     = var.proxy_ui_color
-    "ui/title"     = var.proxy_ui_title
+    "/ui/color"     = var.proxy_ui_color
+    "/ui/title"     = var.proxy_ui_title
   }
 
   subkey {
-    path  = "statsd/rules"
+    path  = "/statsd/rules"
     value = filebase64("${path.module}/statsd_rules.txt")
   }
 
--- consul-kv-service-servicediscovery/main.tf
+++ consul-kv-service-servicediscovery/main.tf
@@ -1,15 +1,15 @@
 resource "consul_key_prefix" "consul-kv-service-servicediscovery" {
   token      = var.consul_acl_token
 
-  path_prefix = "config/services/consul/"
+  path_prefix = "config/services/consul"
 
   subkeys = {
-    "datacenter"  = var.consul_datacenter
-    "domain"      = var.consul_domain
+    "/datacenter"  = var.consul_datacenter
+    "/domain"      = var.consul_domain
   }
 
   subkey {
-    path  = "statsd/rules"
+    path  = "/statsd/rules"
     value = filebase64("${path.module}/statsd_rules.txt")
   }
 
