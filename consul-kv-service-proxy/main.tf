bd5c2a32832213e23f8416d4e4a416ec5e876bb1
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-06-26 16:32:32 +1200

    Add the configuration module for calvinverse proxy

---
+++ consul-kv-service-proxy/main.tf
@@ -0,0 +1,16 @@
+resource "consul_key_prefix" "consul-kv-service-proxy" {
+  token      = var.consul_acl_token
+
+  path_prefix = "config/services/proxy.edge"
+
+  subkeys = {
+    "ui/color"     = var.proxy_ui_color
+    "ui/title"     = var.proxy_ui_title
+  }
+
+  subkey {
+    path  = "statsd/rules"
+    value = filebase64("${path.module}/statsd_rules.txt")
+  }
+
+}
