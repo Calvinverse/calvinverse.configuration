f1550f3311aea7a0a20412336c8b1ec5cfab5bc3
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-06-26 17:30:27 +1200

    Remove the explicit providers because then things break
    if we use depends_on, foreach or others

--- consul-kv-service-servicediscovery/tf_versions.tf
+++ consul-kv-service-servicediscovery/tf_versions.tf
@@ -1,14 +1,3 @@
 terraform {
   required_version = "~> 0.15.4"
-
-  required_providers {
-    consul = {
-      version = "~> 2.12.0"
-    }
-  }
-}
-
-provider "consul" {
-  address = "${var.consul_server_hostname}:${var.consul_server_port}"
-  datacenter = var.consul_datacenter
 }
