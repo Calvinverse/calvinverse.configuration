88d166024ca7c28ed1da64c2b99591f79fee8b44
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-06-26 20:14:08 +1200

    Configure keys one-by-one
    And see if that works

--- consul-kv-service-proxy/main.tf
+++ consul-kv-service-proxy/main.tf
@@ -1,16 +1,30 @@
-resource "consul_key_prefix" "consul-kv-service-proxy" {
+locals {
+  kv_prefix = "config/services/proxy.edge"
+}
+
+resource "consul_key" "proxy-ui-color" {
   token      = var.consul_acl_token
 
-  path_prefix = "config/services/proxy.edge"
-
-  subkeys = {
-    "/ui/color"     = var.proxy_ui_color
-    "/ui/title"     = var.proxy_ui_title
+  key {
+    path = "${local.kv_prefix}/ui/color"
+    value = var.proxy_ui_color
   }
+}
 
-  subkey {
-    path  = "/statsd/rules"
+resource "consul_key" "proxy-ui-title" {
+  token      = var.consul_acl_token
+
+  key {
+    path = "${local.kv_prefix}/ui/title"
+    value = var.proxy_ui_title
+  }
+}
+
+resource "consul_key" "proxy-ui-title" {
+  token      = var.consul_acl_token
+
+  key {
+    path = "${local.kv_prefix}//statsd/rules"
     value = filebase64("${path.module}/statsd_rules.txt")
   }
-
 }
--- consul-kv-service-servicediscovery/main.tf
+++ consul-kv-service-servicediscovery/main.tf
@@ -1,16 +1,30 @@
-resource "consul_key_prefix" "consul-kv-service-servicediscovery" {
+locals {
+  kv_prefix = "config/services/consul"
+}
+
+resource "consul_key" "servicediscovery-datacenter" {
   token      = var.consul_acl_token
 
-  path_prefix = "config/services/consul"
-
-  subkeys = {
-    "/datacenter"  = var.consul_datacenter
-    "/domain"      = var.consul_domain
+  key {
+    path = "${local.kv_prefix}/datacenter"
+    value = var.consul_datacenter
   }
+}
 
-  subkey {
-    path  = "/statsd/rules"
+resource "consul_key" "servicediscovery-domain" {
+  token      = var.consul_acl_token
+
+  key {
+    path = "${local.kv_prefix}/domain"
+    value = var.consul_domain
+  }
+}
+
+resource "consul_key" "servicediscovery-statsd-rules" {
+  token      = var.consul_acl_token
+
+  key {
+    path  = "${local.kv_prefix}/statsd/rules"
     value = filebase64("${path.module}/statsd_rules.txt")
   }
-
 }
